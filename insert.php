8ca37c5986051ba8bf5cf862703908169a97a3b5
Shimodaira0910 <hirokiso83@gmail.com>
2022-08-27 12:00:47 +0900

    first commit

--- insert.php
+++ insert.php
@@ -10,13 +10,13 @@ values('".$_POST['name']."','".$_POST['mail']."','".$_POST['age']."','".$_POST['
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>お問い合わせフォーム</title>
     <link rel="stylesheet" href="style2.css">
 </head>
+
 <body>
 
 <h1>お問い合わせフォーム</h1>
